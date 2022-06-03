a80c5efb7de2894f8a9a58b87642445b5cbe6d91
Jean Santos Silva <40009808+jean07ss@users.noreply.github.com>
2022-06-03 15:05:30 -0300

    First commit and gitignore

---
+++ Forecasts/Controllers/GoodWeatherForecastController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+namespace Forecasts.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class GoodWeatherForecastController : ControllerBase
+{
+    [HttpGet("default-forecast", Name = "GetDefaultWheaterForecast")]
+    public async Task<IActionResult> GetDefault()
+    {
+        string message = "Good weather forecast";
+        return Ok(message);
+    }
+    
+    [HttpGet("temperature", Name = "GetCelsiusTemperature")]
+    public async Task<IActionResult> GetTemperature(int? n1, int? n2)
+    {
+        int temperature = n1 + n2 ?? 77;
+        return Ok(temperature);
+    }
+}
---
+++ ForecastsTests/Integration/GoodWeatherForecastControllerTest.cs
@@ -0,0 +1,65 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+
+namespace ForecastsTests.Integration;
+
+[TestFixture]
+public class GoodWeatherForecastController
+{
+    private HttpClient _httpClient;
+    public GoodWeatherForecastController()
+    {
+        var webAppFactory = new WebApplicationFactory<Program>();
+        _httpClient = webAppFactory.CreateDefaultClient();
+    }
+    
+    [Test]
+    public async Task GetDefaultForecast()
+    {
+        // Arrange
+        var request = "/GoodWeatherForecast/default-forecast";
+        
+        // Act
+        var response = await _httpClient.GetAsync(request);
+        var stringResult = await response.Content.ReadAsStringAsync();
+            
+        // Assert
+        Assert.AreEqual("Good weather forecast", stringResult);
+        Assert.AreEqual(200, (int)response.StatusCode);
+        Assert.AreEqual("OK", response.ReasonPhrase);
+    }
+    
+    [Test]
+    public async Task GetDefaultTemperature()
+    {
+        // Arrange
+        var request = "/GoodWeatherForecast/temperature";
+        
+        // Act
+        var response = await _httpClient.GetAsync(request);
+        var stringResult = await response.Content.ReadAsStringAsync();
+            
+        // Assert
+        Assert.AreEqual(77, int.Parse(stringResult));
+        Assert.AreEqual(200, (int)response.StatusCode);
+        Assert.AreEqual("OK", response.ReasonPhrase);
+    }
+    
+    [Test]
+    public async Task MethodName_WithWhat_ShouldDoWhat()
+    {
+        // Arrange
+        var request = "/GoodWeatherForecast/temperature?n1=1&n2=2";
+        
+        // Act
+        var response = await _httpClient.GetAsync(request);
+        var stringResult = await response.Content.ReadAsStringAsync();
+            
+        // Assert
+        Assert.AreEqual(3, int.Parse(stringResult));
+        Assert.AreEqual(200, (int)response.StatusCode);
+        Assert.AreEqual("OK", response.ReasonPhrase);
+    }
+}
